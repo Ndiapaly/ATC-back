0fff2271e7a5845dd4c7c5adedb11319cde98756
Ndiapaly <jacobndiaye0113gmail@.com>
2025-02-16 17:13:37 +0000

    firt commit

--- app.js
+++ app.js
@@ -66,9 +66,7 @@ app.get('/', (req, res) => {
     res.send('Bienvenue sur l\'API Formulaire');
 });
 
-// Utilisation des routes
-app.use('/inscriptions', inscriptionRoutes);
-
+// Lancement du serveur
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 // Compare this snippet from formulaire/server.js:
